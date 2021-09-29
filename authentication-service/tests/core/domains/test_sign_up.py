215372eba76af2d931c2a302bf1450665ee22a4b
Lucas França <lucasfrancaid@gmail.com>
2021-09-29 04:18:45 -0300

    Add test to unmatched passwords to 100% sign up domain tests coverage

--- authentication-service/tests/core/domains/test_sign_up.py
+++ authentication-service/tests/core/domains/test_sign_up.py
@@ -63,3 +63,17 @@ def test_sign_up_domain_password_confirmation_length_must_be_equal_or_more_than_
     assert error['loc'][0] == 'password_confirmation'
     assert error['msg'] == 'ensure this value has at least 8 characters'
     assert error['type'] == 'value_error.any_str.min_length'
+
+
+def test_sign_up_domain_password_and_password_confirmation_must_be_equal(sign_up_dict: Dict):
+    sign_up_dict['password'] = 'MyPass1234'
+    sign_up_dict['password_confirmation'] = 'MyPass12345'
+
+    with pytest.raises(ValidationError) as exc:
+        SignUpDomain(**sign_up_dict)
+
+    error = literal_eval(exc.value.json())[0]
+
+    assert error['loc'][0] == 'password_confirmation'
+    assert error['msg'] == 'Password and Password Confirmation must be equal'
+    assert error['type'] == 'value_error'
