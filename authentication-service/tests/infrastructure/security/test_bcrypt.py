b66440f192efb3caa891652f06500f512c3e3286
Lucas França <lucasfrancaid@gmail.com>
2021-10-06 04:03:14 -0300

    Add PasswordManagerBcrypt to hash and check passords with bcrypt

---
+++ authentication-service/tests/infrastructure/security/test_bcrypt.py
@@ -0,0 +1,35 @@
+import pytest
+import bcrypt
+from service.core.security.password_manager import PasswordManager
+
+from service.infrastructure.security.bcrypt import PasswordManagerBcrypt
+
+pytest.mock_password = 'Abc123!'
+pytest.mock_bcrypt_salt = bcrypt.gensalt()
+
+
+def test_bcrypt_password_manager_subclass():
+    assert issubclass(PasswordManagerBcrypt, PasswordManager)
+
+
+def test_bcrypt_password_manager_instance_salt():
+    password_manager = PasswordManagerBcrypt(salt=pytest.mock_bcrypt_salt)
+
+    assert password_manager.salt == pytest.mock_bcrypt_salt
+
+
+def test_bcrypt_password_manager_hash_and_check():
+    password_manager = PasswordManagerBcrypt(salt=pytest.mock_bcrypt_salt)
+
+    hash_password = password_manager.hash(password=pytest.mock_password)
+
+    assert isinstance(hash_password, bytes)
+    assert password_manager.check(password=pytest.mock_password, hash_password=hash_password) is True
+
+
+def test_bcrypt_password_manager_hash_and_check_different_password_must_be_false():
+    wrong_password = 'Xyz456!'
+    password_manager = PasswordManagerBcrypt(salt=pytest.mock_bcrypt_salt)
+
+    hash_password = password_manager.hash(password=pytest.mock_password)
+    assert password_manager.check(password=wrong_password, hash_password=hash_password) is False
